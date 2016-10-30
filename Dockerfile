a2649b9b1420a9761cc1a142e9a9905ae70929a3
Lukas Rosenstock <lr@lukasrosenstock.net>
2016-10-30 19:23:14 +0100

    Initial code commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM centos:7
+MAINTAINER "Lukas Rosenstock"
+
+# Enable EPEL repository (required for lighttpd)
+RUN rpm -Uvh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# Enable Webtatic repository (required for PHP7)
+RUN rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+
+# Install all required packages through yum
+RUN yum install -y --exclude=httpd --skip-broken lighttpd lighttpd-fastcgi \
+  php70w php70w-opcache php70w-common php70w-pecl-apcu php70w-pdo php70w-mysql \
+  php70w-xml php70w-mbstring php70w-gd unzip
+
+# Add configuration files
+ADD lighttpd.conf.php /tmp/
+
+# Open HTTP Port
+EXPOSE 80
+
+# Set up starter script
+ADD start.sh /tmp/
+
+# Launch starter script
+CMD ["/bin/sh", "/tmp/start.sh"]
---
+++ lighttpd.conf.php
@@ -0,0 +1,67 @@
+<?php
+
+// Dynamically divide number of desired parallel PHP processes
+// into parent and child processes
+
+$procs = isset($argv[1]) && (int)$argv[1]>=1 ? (int)$argv[1] : 2;
+$parentProcs = min(4, ($procs % 3 == 0) ? $procs/3 : ceil($procs/2));
+$childProcs = ceil($procs/$parentProcs);
+
+?>
+
+## lighttpd configuration file
+
+## Modules to load
+
+server.modules = ("mod_rewrite", "mod_fastcgi", "mod_accesslog")
+
+## where to send error-messages to
+server.errorlog             = "/var/log/lighttpd/error.log"
+
+## send a different Server: header
+## be nice and keep it at lighttpd
+server.tag                 = "lighttpd"
+
+#### accesslog module
+accesslog.filename          = "/var/log/lighttpd/access.log"
+
+## to help the rc.scripts
+server.pid-file            = "/var/run/lighttpd.pid"
+
+# change uid to <uid> (default: don't care)
+server.username            = "lighttpd"
+# change uid to <uid> (default: don't care)
+server.groupname           = "lighttpd"
+
+## FastCGI Configuration
+fastcgi.server = ( ".php" => (
+	"localhost" => (
+			"socket" => "/var/run/lighttpd/php-fastcgi.socket",
+			"bin-path" => "/usr/bin/php-cgi",
+			"max_procs" => <?php echo $parentProcs; ?>,
+			"bin-environment" => (
+				"PHP_FCGI_CHILDREN" => "<?php echo $childProcs; ?>",
+				"PHP_FCGI_MAX_REQUESTS" => "500"
+			)
+		)
+	)
+)
+
+# MIME Types for static files
+mimetype.assign  = (
+  ".gif"          =>      "image/gif",
+  ".jpg"          =>      "image/jpeg",
+  ".jpeg"         =>      "image/jpeg",
+  ".png"          =>      "image/png",
+  ".css"          =>      "text/css",
+  ".js"           =>      "text/javascript"
+)
+
+# Solve problem with 417 errors
+server.reject-expect-100-with-417 = "disable"
+
+# URL Rewriting
+url.rewrite-if-not-file = ( "^/(.*)" => "/index.php/$1" )
+
+# Document Root
+server.document-root = "/var/www/app/web/"
